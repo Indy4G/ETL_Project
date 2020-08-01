7d994b83bce8fe9d84f439d31043088d84ac3450
Indy4G <63853278+Indy4G@users.noreply.github.com>
2020-08-01 12:28:59 -0500

    completed ETL Project

---
+++ austin_beer_database.sql
@@ -0,0 +1,25 @@
+-- create table austin_breweries (
+-- 	id serial primary key not null,
+-- 	Brewery varchar(300),
+-- 	Street_Address varchar(300),
+-- 	Zipcode int,
+-- 	Austin_Area varchar(300)
+-- );
+
+-- select * from austin_breweries;
+
+-- create table beers (
+-- 	id serial primary key not null,
+-- 	Brewery_ID int not null,
+-- 	foreign key (Brewery_ID) references austin_breweries(id),
+-- 	Name varchar(300),
+-- 	Type varchar(300),
+-- 	ABV float,
+-- 	IBU float,
+-- 	Rating float,
+-- 	Rating_Count int
+-- );
+
+-- select * from beers;
+
+
